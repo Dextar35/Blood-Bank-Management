c9e0d38d6ba3814350547430cc67ae3c9e4c9036
Dextar35 <143207615+Dextar35@users.noreply.github.com>
2024-09-27 23:45:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -215,7 +215,7 @@ app.get('/search', async (req, res) => {
 
 
 // Start the server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
